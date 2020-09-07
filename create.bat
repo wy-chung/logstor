e546094c54aa905005158630466edc607babea05
Wuyang Chung <wuyang.chung1@outlook.com>
2020-09-07 21:20:11 +0800

    * Rename dd.bat to init.bat. * Add create.bat.

---
+++ create.bat
@@ -0,0 +1,4 @@
+newfs /dev/ggate0
+tunefs -t enable /dev/ggate0
+mount /dev/ggate0 /mnt
+
--- init.bat
+++ init.bat
@@ -4,7 +4,4 @@ setenv target /dev/da4s2
 
 dd if=/dev/zero of=$target bs=32k count=1
 ./ggatelog create $target
-#newfs /dev/ggate0
-#tunefs -t enable /dev/ggate0
-#mount /dev/ggate0 /mnt
 
